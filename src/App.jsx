1277f4c160f4fc8124012a6ce6dc712c69712106
Matatcoding <matthew_whitlock@comcast.net>
2025-07-31 13:30:53 -0400

    finally figured out error so I can finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
+import { useState } from "react";
+import ShowDetails from "./shows/ShowDetails";
+import ShowSelection from "./shows/ShowSelection";
+import { tvShows } from "./shows/data";
 /**
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
 export default function App() {
+  const [selectedShow, setSelectedShow] = useState();
   return (
     <>
       <header>
         <p>React TV</p>
+        <ShowSelection shows={tvShows} setSelectedShow={setSelectedShow} />
       </header>
-      <main></main>
+      <main>
+        <ShowDetails show={selectedShow} />
+      </main>
     </>
   );
 }
